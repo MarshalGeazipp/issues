d574c8f46831d0cebb5d0195fba40306a9b96358
Sebastian Obermüller <Sebastian.Obermueller@1und1.de>
2017-02-02 09:19:03 +0100

    tiny indentations fix

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -19,8 +19,8 @@ defmodule Issues.CLI do
 	@doc """
 	`argv` can be -h or --help, which returns :help.
 
-  	Otherwise it is a github user name, project name, and (optionally)
-  	the number of entries to format.
+  Otherwise it is a github user name, project name, and (optionally)
+  the number of entries to format.
 
 	Return a tuple of `{ user, project, count }`, or `:help` if help was given. 
 	"""

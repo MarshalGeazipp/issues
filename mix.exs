8539ba1bc4a8a0004d0273915f6538b89c4a8997
Sebastian Obermüller <Sebastian.Obermueller@1und1.de>
2017-02-03 10:08:31 +0100

    added doctest and code coverage tools

--- mix.exs
+++ mix.exs
@@ -10,6 +10,13 @@ defmodule Issues.Mixfile do
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     test_coverage: [tool: ExCoveralls],
+     preferred_cli_env: [
+       "coveralls": :test,
+       "coveralls.detail": :test,
+       "coveralls.post": :test,
+       "coveralls.html": :test
+     ],
      deps: deps()]
   end
 
@@ -35,7 +42,8 @@ defmodule Issues.Mixfile do
       { :httpoison, "~> 0.9" },
       { :poison,    "~> 3.1" },
       { :ex_doc,    "~> 0.12"},
-      { :earmark,   "~> 1.0", override: true}
+      { :earmark,   "~> 1.0", override: true},
+      { :excoveralls, "~> 0.5", only: :test}
     ]
   end
 
